dfaf543de07eb5f0c01b1d938f6f2fee94919fe7
Sanderson Coelho <sanderson.mc@gmail.com>
2022-01-21 20:27:29 -0300

    Add all fields to CurrencyTransaction

--- src/main/java/org/example/currencyconversionapi/model/CurrencyTransaction.java
+++ src/main/java/org/example/currencyconversionapi/model/CurrencyTransaction.java
@@ -1,8 +1,16 @@
 package org.example.currencyconversionapi.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class CurrencyTransaction {
     private Long currencyTransactionId;
     private Long userId;
+    private String originCurrency;
+    private BigDecimal originValue;
+    private String destinationCurrency;
+    private BigDecimal conversionRate;
+    private LocalDateTime dateTimeTransaction;
 
     public Long getCurrencyTransactionId() {
         return currencyTransactionId;
@@ -18,5 +26,45 @@ public class CurrencyTransaction {
 
     public void setUserId(Long userId) {
         this.userId = userId;
+    }
+
+    public String getOriginCurrency() {
+        return originCurrency;
+    }
+
+    public void setOriginCurrency(String originCurrency) {
+        this.originCurrency = originCurrency;
+    }
+
+    public BigDecimal getOriginValue() {
+        return originValue;
+    }
+
+    public void setOriginValue(BigDecimal originValue) {
+        this.originValue = originValue;
+    }
+
+    public String getDestinationCurrency() {
+        return destinationCurrency;
+    }
+
+    public void setDestinationCurrency(String destinationCurrency) {
+        this.destinationCurrency = destinationCurrency;
+    }
+
+    public BigDecimal getConversionRate() {
+        return conversionRate;
+    }
+
+    public void setConversionRate(BigDecimal conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+
+    public LocalDateTime getDateTimeTransaction() {
+        return dateTimeTransaction;
+    }
+
+    public void setDateTimeTransaction(LocalDateTime dateTimeTransaction) {
+        this.dateTimeTransaction = dateTimeTransaction;
     }
 }
