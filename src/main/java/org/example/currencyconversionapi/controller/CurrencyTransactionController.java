b268b4048a4a5e3867bb5704352f1c794f1e3af2
Sanderson Coelho <sanderson.mc@gmail.com>
2022-01-23 14:25:04 -0300

    Return code according its exception

--- src/main/java/org/example/currencyconversionapi/controller/CurrencyTransactionController.java
+++ src/main/java/org/example/currencyconversionapi/controller/CurrencyTransactionController.java
@@ -1,8 +1,12 @@
 package org.example.currencyconversionapi.controller;
 
+import org.example.currencyconversionapi.exceptions.ExchangeRateApiException;
+import org.example.currencyconversionapi.exceptions.NoSuchCurrencyException;
 import org.example.currencyconversionapi.model.CurrencyTransaction;
+import org.example.currencyconversionapi.model.CurrencyTransactionResponse;
 import org.example.currencyconversionapi.service.CurrencyTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -17,11 +21,21 @@ public class CurrencyTransactionController {
     private CurrencyTransactionService currencyTransactionService;
 
     @GetMapping("/conversion")
-    public Mono<CurrencyTransaction> makeConversion(@RequestParam("userId") Long userId,
-                                                    @RequestParam("originCurrency") String originCurrency,
-                                                    @RequestParam("originValue") BigDecimal originValue,
-                                                    @RequestParam("destinationCurrency") String destinationCurrency) {
-        return currencyTransactionService.makeConversion(userId, originCurrency, originValue, destinationCurrency);
+    public Mono<CurrencyTransactionResponse> makeConversion(@RequestParam("userId") Long userId,
+                                                            @RequestParam("originCurrency") String originCurrency,
+                                                            @RequestParam("originValue") BigDecimal originValue,
+                                                            @RequestParam("destinationCurrency") String destinationCurrency) {
+        return currencyTransactionService
+                .makeConversion(userId, originCurrency, originValue, destinationCurrency)
+                .flatMap(currencyTransaction ->
+                        Mono.just(new CurrencyTransactionResponse(
+                                HttpStatus.OK.value(), null, currencyTransaction)))
+                .onErrorResume(NoSuchCurrencyException.class, e ->
+                        Mono.just(new CurrencyTransactionResponse(
+                                HttpStatus.BAD_REQUEST.value(), e.getMessage(), null)))
+                .onErrorResume(ExchangeRateApiException.class, e ->
+                        Mono.just(new CurrencyTransactionResponse(
+                                HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), null)));
     }
 
     @GetMapping("/user/{userId}")
--- src/test/java/org/example/currencyconversionapi/controller/CurrencyTransactionControllerTest.java
+++ src/test/java/org/example/currencyconversionapi/controller/CurrencyTransactionControllerTest.java
@@ -1,13 +1,17 @@
 package org.example.currencyconversionapi.controller;
 
 import org.example.currencyconversionapi.enums.Currency;
+import org.example.currencyconversionapi.exceptions.ExchangeRateApiException;
+import org.example.currencyconversionapi.exceptions.NoSuchCurrencyException;
 import org.example.currencyconversionapi.model.CurrencyTransaction;
+import org.example.currencyconversionapi.model.CurrencyTransactionResponse;
 import org.example.currencyconversionapi.service.CurrencyTransactionService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
@@ -16,6 +20,9 @@ import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
@@ -49,7 +56,62 @@ public class CurrencyTransactionControllerTest {
                 .exchange()
                 .expectStatus()
                 .isOk()
-                .expectBody(CurrencyTransaction.class);
+                .expectBody(CurrencyTransactionResponse.class)
+                .value(currencyTransactionResponse -> {
+                    assertEquals(HttpStatus.OK.value(), currencyTransactionResponse.getStatus());
+                    assertNull(currencyTransactionResponse.getMessage());
+                    assertNotNull(currencyTransactionResponse.getData());
+                });
+    }
+
+    @Test
+    public void makeConversionFailWithNoSuchCurrencyTest() {
+        String messageException = "Invalid currency";
+        when(currencyTransactionService.makeConversion(userId, originCurrency, originValue, destinationCurrency))
+                .thenReturn(Mono.error(new NoSuchCurrencyException(messageException)));
+
+        webClient
+                .get().uri(uriBuilder -> uriBuilder
+                .path("/currency-transaction/conversion")
+                .queryParam("userId", userId)
+                .queryParam("originCurrency", originCurrency)
+                .queryParam("originValue", originValue)
+                .queryParam("destinationCurrency", destinationCurrency)
+                .build())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(CurrencyTransactionResponse.class)
+                .value(currencyTransactionResponse -> {
+                    assertEquals(HttpStatus.BAD_REQUEST.value(), currencyTransactionResponse.getStatus());
+                    assertEquals(messageException, currencyTransactionResponse.getMessage());
+                    assertNull(currencyTransactionResponse.getData());
+                });
+    }
+
+    @Test
+    public void makeConversionFailOnConnectToExchangeRateApiTest() {
+        String messageException = "Failed on connecting to ExchangeRateApi";
+        when(currencyTransactionService.makeConversion(userId, originCurrency, originValue, destinationCurrency))
+                .thenReturn(Mono.error(new ExchangeRateApiException(messageException)));
+
+        webClient
+                .get().uri(uriBuilder -> uriBuilder
+                .path("/currency-transaction/conversion")
+                .queryParam("userId", userId)
+                .queryParam("originCurrency", originCurrency)
+                .queryParam("originValue", originValue)
+                .queryParam("destinationCurrency", destinationCurrency)
+                .build())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(CurrencyTransactionResponse.class)
+                .value(currencyTransactionResponse -> {
+                    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), currencyTransactionResponse.getStatus());
+                    assertEquals(messageException, currencyTransactionResponse.getMessage());
+                    assertNull(currencyTransactionResponse.getData());
+                });
     }
 
     @Test
